88e3e7cd194376f555070315fe878ae105595f85
friday <eunbiryung@gmail.com>
2020-12-09 03:57:11 +0900

    미들웨어 함수 등록하기

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var http = require('http');
 
 var app = express();
 
-app.set('port', process.env.ports || 3000);
+app.set('port', process.env.PORT || 3000);
 
 var server = http.createServer(app).listen(app.get('port'), function(){
     console.log('익스프레스로 웹 서버를 실행함 : ' + app.get('port'))
---
+++ app2.js
@@ -0,0 +1,18 @@
+// 미들웨어 이해하기
+
+var express = require('express');
+var http = require('http');
+
+var app = express();
+
+app.set('port', process.env.PORT || 3000);
+
+app.use(function(req, res, next){
+    console.log("첫번째로 등록한 미들웨어 함수가 호출됨")
+    res.writeHead(200, {'Content-Type':'text/html;charset=utf8'});
+    res.end('<h1>서버에서 응답한 결과입니다.</h1>');
+})
+
+var server = http.createServer(app).listen(app.get('port'), function(){
+    console.log("익스프레스로 웹 서버를 실행함 : " + app.get('port'))
+})
