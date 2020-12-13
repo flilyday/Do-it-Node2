1cf0ae78dc9bd7a94174c231bb4659343a113a44
friday <eunbiryung@gmail.com>
2020-12-13 21:30:43 +0900

    요청파라미터 확인하기

--- app6.js
+++ app6.js
@@ -1,4 +1,4 @@
-//리다이렉트 시키기
+//요청 파라미터 확인하기
 var express = require('express')
 var http = require('http')
 
@@ -10,7 +10,10 @@ app.set('port', process.env.port || 3000)
 app.use(function(req, res, next){
     console.log('첫번째 미들웨어 함수가 실행됨')
 
-    res.redirect('http://www.google.com')
+    var userAgent = req.header('User-Agent')
+    var paramName = req.query.name
+
+    res.send('<h3>서버에서 응답. User-Agent -> </h3>' + userAgent + '</h3><h3>Param Name -> ' + paramName + '</h3>')
 })
 
 var server = http.createServer(app).listen(app.get('port'), function(){
