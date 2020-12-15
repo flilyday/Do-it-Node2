248e1278c023b0729870dfe12165ce63f65ab908
snupy <eunbiryung@gmail.com>
2020-12-15 12:16:15 +0900

    cookie사용하기

--- app11.js
+++ app11.js
@@ -1,9 +1,10 @@
-// post로 받은 변수 처리
+// cookie 사용하기
 var express = require('express');
 var http = require('http');
 var static = require('serve-static');
 var path = require('path');
 var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
 
 var app = express();
 
@@ -13,19 +14,24 @@ app.use('/public', static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
+app.use(cookieParser());
+
 var router = express.Router();
 
-router.route('/process/login').post(function(req, res){
-    console.log('/process/login 라우팅 함수에서 받음.')
+router.route('/process/setUserCookie').get(function(req, res){
+    console.log('/process/setUserCookie 라우팅 함수 호출됨')
+    res.cookie('user',{
+        id:'mikde',
+        name:'sosi',
+        authorized:true
+    })
+    res.redirect('/process/showCookie')
+});
 
-    var paramId = req.body.id || req.query.id;
-    var paramPassword = req.body.password || req.query.password;
+router.route('/process/showCookie').get(function(req, res){
+    console.log('/process/showCookie 라우팅 함수 호출됨');
 
-    res.writeHead(200, {"Content-Type":"text/html;charset=utf8"});
-    res.write("<h1>서버에서 로그인 응답</h1>");
-    res.write("<div><p>" + paramId + "</p></div>");
-    res.write("<div><p>" + paramPassword + "</p></div>");
-    res.end();
+    res.send(req.cookies);
 })
 
 app.use('/', router);
